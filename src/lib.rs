80046b0409b3a872f66c1352ea8b90b67f82b63d
Eliza Weisman <eliza@buoyant.io>
2017-09-11 10:14:12 -0500

    Start on core types

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
+use std::path::PathBuf;
+
+#[derive(Debug)]
+pub struct Event {
+    path: Option<PathBuf>,
+    kind: EventKind,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum EventKind {
+    Created,
+    Deleted,
+    AttrsModified,
+    WriteOpen,
+    WriteClosed,
+    Renamed,
 }
